e63209a28823df8091c1b09768cd97b2442d3e2c
Emanoel Borges <rodbemanoelstudies@gmail.com>
2025-06-18 14:37:06 -0300

    Começando a implementação da calculadora

---
+++ main.py
@@ -0,0 +1,17 @@
+#Projeto final Matemática Financeira
+#Aluno: Emanoel Borges
+#Professora: Mara
+
+def main():
+
+    print("--- Calculadora Financeira ---")
+    print("Escolha uma opcao:")
+    print("1 - Juros Simples ")
+
+    opcao = int(input("\nDigite a opcao: "))
+
+    if opcao == 1:
+        print("Vamos calcular o juros simples!")
+        vf = float(input("Digite o valor: R$")) 
+
+main()
