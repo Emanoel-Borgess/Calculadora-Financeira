7d6aa210360a59bf6eb0f350ff4d9f2386dbaec0
Emanoel Borges <rodbemanoelstudies@gmail.com>
2025-06-21 06:55:57 -0300

    Opção 1 do menu implementada

--- main.py
+++ main.py
@@ -2,16 +2,31 @@
 #Aluno: Emanoel Borges
 #Professora: Mara
 
+def jurosSimples(vp, i, n):
+    j = vp * i * n
+    return j
+
+def valorFuturo(vp, i, n):
+    vf = vp *(1 + i * n)
+    return vf
+
 def main():
 
     print("--- Calculadora Financeira ---")
     print("Escolha uma opcao:")
-    print("1 - Juros Simples ")
+    print("1 - Calcular juros simples ")
 
-    opcao = int(input("\nDigite a opcao: "))
+    opcao = int(input("\nDigite a opcao (1 a ?): "))
 
     if opcao == 1:
         print("Vamos calcular o juros simples!")
-        vf = float(input("Digite o valor: R$")) 
+        vp = float(input("Digite o valor presente R$ "))
+        i = float(input("Digite a taxa: "))
+        n = float(input("Digite o tempo: "))
+        juros = jurosSimples(vp, i, n)
+        print(f"O valor do juros simples é: R$ {juros:.2f}")
+        print(f"O valor futuro é: R$ {valorFuturo(vp, i, n):.2f}")
+    else:
+        print("Opção inválida. Tente novamente.")
 
 main()
