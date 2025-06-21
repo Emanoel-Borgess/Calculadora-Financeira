463a9a570b31ee3bda743608d7dd4ae2972681cd
Emanoel Borges <rodbemanoelstudies@gmail.com>
2025-06-21 08:09:54 -0300

    Implementei o while no código

--- main.py
+++ main.py
@@ -7,26 +7,34 @@ def jurosSimples(vp, i, n):
     return j
 
 def valorFuturo(vp, i, n):
-    vf = vp *(1 + i * n)
+    vf = vp * (1 + i * n)
     return vf
 
 def main():
+    
+    while True:
+    
+        print("--- Calculadora Financeira ---")
+        print("Escolha uma opcao:")
+        print("1 - Calcular juros simples ")
+        print("0 - Sair ")
 
-    print("--- Calculadora Financeira ---")
-    print("Escolha uma opcao:")
-    print("1 - Calcular juros simples ")
+        opcao = int(input("\nDigite a opcao (1 a ?): "))
 
-    opcao = int(input("\nDigite a opcao (1 a ?): "))
+        if opcao == 1:
+            print("Vamos calcular o juros simples!")
+            vp = float(input("Digite o valor presente R$ "))
+            i = float(input("Digite a taxa: "))
+            n = float(input("Digite o tempo: "))
+            juros = jurosSimples(vp, i, n)
+            print(f"O valor do juros simples é: R$ {juros:.2f}")
+            print(f"O valor futuro é: R$ {valorFuturo(vp, i, n):.2f}") 
 
-    if opcao == 1:
-        print("Vamos calcular o juros simples!")
-        vp = float(input("Digite o valor presente R$ "))
-        i = float(input("Digite a taxa: "))
-        n = float(input("Digite o tempo: "))
-        juros = jurosSimples(vp, i, n)
-        print(f"O valor do juros simples é: R$ {juros:.2f}")
-        print(f"O valor futuro é: R$ {valorFuturo(vp, i, n):.2f}")
-    else:
-        print("Opção inválida. Tente novamente.")
+        elif opcao == 0:
+            print("Saindo...")
+            break
+
+        else:
+            print("Opção inválida. Tente novamente.\n")
 
 main()
